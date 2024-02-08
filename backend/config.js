daf86597bf5430ce45da06fd2a203042f453115f
Priya Rathore <rathorepriya1705@gmail.com>
2024-02-09 01:15:03 +0530

    Project completion

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 8000;
+
+export const URL =
+  "mongodb+srv://PriyaRathore:PasswordIs123@book-store.7xfx5sm.mongodb.net/books-collection?retryWrites=true&w=majority";
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,61 @@
+import axios from "axios";
+import Spinner from "../components/Spinner";
+import { Link } from "react-router-dom";
+import { MdOutlineAddBox } from "react-icons/md";
+import BooksTable from "../components/home/BooksTable";
+import BooksCard from "../components/home/BooksCard";
+import { useEffect, useState } from "react";
+
+const Home = () => {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [showType, setShowType] = useState("table");
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("http://localhost:8000/books")
+      .then((response) => {
+        setBooks(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="p-4">
+      <div className="flex justify-between items-center">
+        <h1 className="text-4xl font-bold my-8">Books List</h1>
+        <Link to="/books/create">
+          <MdOutlineAddBox className="text-sky-800 text-4xl" />
+        </Link>
+      </div>
+      <div className="flex justify-center items-center gap-x-4 mb-6">
+        <button
+          className="w-24 h-10 text-black hover:text-white bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
+          onClick={() => setShowType("table")}
+        >
+          Table
+        </button>
+        <button
+          className="w-24 h-10 text-black hover:text-white bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
+          onClick={() => setShowType("card")}
+        >
+          Card
+        </button>
+      </div>
+      {loading ? (
+        <Spinner />
+      ) : showType === "table" ? (
+        <BooksTable books={books} />
+      ) : (
+        <BooksCard books={books} />
+      )}
+    </div>
+  );
+};
+
+export default Home;
